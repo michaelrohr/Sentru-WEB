c0f4bb45c0137e794f8afda937c488c17db6b0a6
michaelrohr <michael.rohr@bluewin.ch>
2015-03-31 16:50:05 +0200

    first

---
+++ module/Sentru/config/module.config.php
@@ -0,0 +1,115 @@
+<?php
+
+/*
+ * 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Sentru\Controller\Sentru' => 'Sentru\Controller\SentruController',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'sentru' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/sentru',
+                    'defaults' => array(
+                        'controller' => 'Sentru\Controller\Sentru',
+                        'action' => 'index',
+                    ),
+                ),
+            ),
+            'about' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/sentru/about',
+                    'defaults' => array(
+                        'controller' => 'Sentru\Controller\Sentru',
+                        'action' => 'about',
+                    ),
+                ),
+            ),
+            'contact' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/sentru/contact',
+                    'defaults' => array(
+                        'controller' => 'Sentru\Controller\Sentru',
+                        'action' => 'contact',
+                    ),
+                ),
+            ),
+            'impulsdays' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/sentru/impulsdays',
+                    'defaults' => array(
+                        'controller' => 'Sentru\Controller\Sentru',
+                        'action' => 'impulsdays',
+                    ),
+                ),
+            ),
+            'leadership' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/sentru/leadership',
+                    'defaults' => array(
+                        'controller' => 'Sentru\Controller\Sentru',
+                        'action' => 'leadership',
+                    ),
+                ),
+            ),
+            'seminare' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/sentru/seminare',
+                    'defaults' => array(
+                        'controller' => 'Sentru\Controller\Sentru',
+                        'action' => 'seminare',
+                    ),
+                ),
+            ),
+            'teambuildung' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/sentru/teambuidling',
+                    'defaults' => array(
+                        'controller' => 'Sentru\Controller\Sentru',
+                        'action' => 'teambuilding',
+                    ),
+                ),
+            ),
+            'location' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/sentru/location',
+                    'defaults' => array(
+                        'controller' => 'Sentru\Controller\Sentru',
+                        'action' => 'location',
+                    ),
+                ),
+            ),
+            'home' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/sentru',
+                    'defaults' => array(
+                        'controller' => 'Sentru\Controller\Sentru',
+                        'action' => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'Sentru' => __DIR__ . '/../view',
+        ),
+    ),
+);
---
+++ vendor/zf-hipsters/bootstrap-flash-messenger/README.md
@@ -0,0 +1,67 @@
+Bootstrap Flash Messenger
+====================
+
+A simple module that makes flash messengers as simple as they are in CakePHP. And the best part is it generates beautifully formatted Bootstrap alerts!
+
+Installation
+--------------
+1) Add the following requirement to your projects composer.json file.
+
+Within the "require" section:
+
+```php
+"zf-hipsters/bootstrap-flash-messenger": ">=1.0"
+```
+
+2) Open up your command line and run
+
+```
+php ./composer.phar update
+```
+
+2) Add 'FlashMessenger' to your /config/application.config.php modules
+
+
+Controller Plugin Examples
+--------------------------
+
+Simple success (this is the default namespace)
+```php
+$this->fm('You have been logged in.');
+return $this->redirect()->toRoute('dashboard');
+```
+
+Error
+```php
+$this->fm('Your username and/or password were incorrect.', 'error');
+return $this->redirect()->toRoute('authorize/login');
+```
+
+Info
+```php
+$this->fm('Something cool happened!', 'info');
+return $this->redirect()->toRoute('home');
+```
+
+Warning
+```php
+$this->fm('Careful! Something bad could happen!', 'warning');
+return $this->redirect()->toRoute('dashboard');
+```
+
+View Helper Example
+--------------------
+
+Default usage
+```php
+<?=$this->fm()?>
+```
+
+Custom Namespace(s)
+```php
+<?=$this->fm('exciting_namespace')?>
+```
+
+```php
+<?=$this->fm(array('mynamespace1', 'mynamespace2'))?>
+```
